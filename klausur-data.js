59777e2a582f3f7b3ca10352983e3a06e5d4aa75
sxty <155656794+sxty9@users.noreply.github.com>
2026-06-02 19:26:02 +0200

    Klausur-App: klausur-data.js aktualisiert

--- klausur-data.js
+++ klausur-data.js
@@ -418,9 +418,13 @@ window.KLAUSUR_DATA = {
       "id": "dgd6x4ef710x",
       "text": "Management ist ein totes Pferd.",
       "height": "2px"
+    },
+    {
+      "id": "pqgk48ngsc4g",
+      "text": "sdsdsdsdsd"
     }
   ],
   "_meta": {
-    "lastSaved": "2026-06-02T17:25:42.614Z"
+    "lastSaved": "2026-06-02T17:26:01.632Z"
   }
 };
